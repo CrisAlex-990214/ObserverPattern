7abed358b70358046ede95a4156085f46125a1f3
CrisAlex <cads.14alex@outlook.com>
2023-10-26 11:13:21 -0500

    First commit

---
+++ Implementation.cs
@@ -0,0 +1,57 @@
+﻿namespace ObserverPattern
+{
+    public class PromoDiscount
+    {
+        public string Product { get; set; }
+        public string Code { get; set; }
+        public int DiscountPercentage { get; set; }
+    }
+
+    //Subject (Observable)
+    public abstract class PromoDiscountNotifier
+    {
+
+        private List<IPromoDiscountListener> observers = new();
+
+        public void AddObserver(IPromoDiscountListener observer) => observers.Add(observer);
+        public void RemoveObserver(IPromoDiscountListener observer) => observers.Remove(observer);
+
+        public async Task Notify(PromoDiscount promoDiscount)
+        {
+            await Task.WhenAll(observers.Select(x => x.Update(promoDiscount)));
+        }
+    }
+
+    //Observer (Consumer)
+    public interface IPromoDiscountListener
+    {
+        Task Update(PromoDiscount promoDiscount);
+    }
+
+    //Concrete Observer
+    public class WebsiteConsumer : IPromoDiscountListener
+    {
+        public async Task Update(PromoDiscount promoDiscount)
+        {
+            await Task.Delay(3000);
+            Console.WriteLine($"{nameof(WebsiteConsumer)} -> You have a promo code: ${promoDiscount.Code} with a " +
+                $"{promoDiscount.DiscountPercentage}% discount for the product {promoDiscount.Product}");
+        }
+    }
+
+    public class NewsletterConsumer : IPromoDiscountListener
+    {
+        public async Task Update(PromoDiscount promoDiscount)
+        {
+            await Task.Delay(1000);
+            Console.WriteLine($"{nameof(NewsletterConsumer)} -> You have a promo code: ${promoDiscount.Code} with a " +
+                $"{promoDiscount.DiscountPercentage}% discount for the product {promoDiscount.Product}");
+        }
+    }
+
+    //Concrete Subject
+    public class PromoDiscountService : PromoDiscountNotifier
+    {
+        public async Task SendPromoNotification(PromoDiscount promoDiscount) => await Notify(promoDiscount);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+using ObserverPattern;
+
+var promoDiscountService = new PromoDiscountService();
+
+var websiteConsumer = new WebsiteConsumer();
+var newsletterConsumer = new NewsletterConsumer();
+
+promoDiscountService.AddObserver(websiteConsumer);
+promoDiscountService.AddObserver(newsletterConsumer);
+
+await promoDiscountService.Notify(new() { Code = "A9F", DiscountPercentage = 30, Product = "Computer" });
+
+promoDiscountService.RemoveObserver(websiteConsumer);
+Console.WriteLine("The Website consumer has been removed!");
+
+await promoDiscountService.Notify(new() { Code = "A9F", DiscountPercentage = 30, Product = "Computer" });
+
+Console.ReadKey();
